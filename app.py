b61eee93b08416faca0d5ab25024539ba5c04925
atwenzel <atwenzel@eng.ucsd.edu>
2024-06-08 16:12:19 -0700

    secret key testing - don't print it

--- app.py
+++ app.py
@@ -8,5 +8,8 @@ app.config.from_object(env_config)
 @app.route('/')
 def index():
 	secret_key = app.config.get("MTS_API_KEY")
-	return f"Configure MTS API key: {secret_key}"
+	if secret_key is None:
+		raise ValueError("Missing API key for MTS live feed")
+
+	return f"Length of secret key is {len(secret_key)}"
 
